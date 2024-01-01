eef043eabbf89ac55878beaeba7cea5dd66a93b2
Snezroamia <1221302721@student.mmu.edu.my>
2024-01-02 00:21:43 +0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,11 +8,6 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-
-			define: {
-			  'process.env': process.env,
-			},
-
 	},
 
 	// hydrate the <div id="svelte"> element in src/app.html
