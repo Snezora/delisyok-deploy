7dfc3bf428d5ad519a173318083059cc8a5e75bc
Snezroamia <1221302721@student.mmu.edu.my>
2024-01-02 23:47:16 +0800

    Update +layout.svelte

--- src/routes/client/dashboard/vendor/+layout.svelte
+++ src/routes/client/dashboard/vendor/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
 	import { supabaseClient } from '$lib/supabase';
 	import { onMount } from 'svelte';
+	import { vendorStore } from '../../../stores/businessStore';
 
     let user;
+    /**
+	 * @type {string | undefined}
+	 */
     let user_id;
     /**
 	 * @type {any[] | null}
@@ -21,6 +25,7 @@
     
     onMount(async () => {
         supabaseClient.auth.onAuthStateChange(async (_, session) => {
+            console.log("onmount in layout triggered");
             user = !!session?.user;
             if (user) {
                 const userLog = await supabaseClient.auth.getUser();
@@ -47,9 +52,48 @@
             }
         });
 
-        
+
+        const vendorData = await fetchBusinessName();
+        console.log(vendorData);
+        const days = JSON.parse(vendorData.businessopday);
+        const openDays = {
+            openMonday: days.includes('Monday'),
+            openTuesday: days.includes('Tuesday'),
+            openWednesday: days.includes('Wednesday'),
+            openThursday: days.includes('Thursday'),
+            openFriday: days.includes('Friday'),
+            openSaturday: days.includes('Saturday'),
+            openSunday: days.includes('Sunday'),
+        };
+        console.log("onmount in layout triggered 2");
+
+        // Update the store
+        vendorStore.set({
+            ...vendorData,
+            ...openDays,
+            user_id
+        });
+    
+
     });
 
+    async function fetchBusinessName() {
+
+        let vendorData = null;
+        
+    // Fetch the businessname from the vendor table
+    const { data, error } = await supabaseClient
+        .from('vendor')
+        .select('*')
+        .eq('user_id', user_id);
+
+    if (error) {
+        console.error('Error fetching business name:', error);
+    } else if (data && data.length > 0) {
+         vendorData = data[0];
+    }
+    return vendorData;
+    }
 
 
 </script>
