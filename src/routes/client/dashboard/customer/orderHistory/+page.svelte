170aa98e0bf0cf9dd9a0c26731eaa59c209fbae1
Ren <125994044+R3nT@users.noreply.github.com>
2024-01-23 17:05:53 +0800

    help with order item :)

--- src/routes/client/dashboard/customer/orderHistory/+page.svelte
+++ src/routes/client/dashboard/customer/orderHistory/+page.svelte
@@ -1,72 +1,134 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    import { supabaseClient } from "$lib/supabase";
-    import { onMount } from 'svelte';
-    import TrashBin from './trash.svelte';
-    import  { Button, Modal } from 'flowbite-svelte';
-    import {ExclamationCircleOutline} from 'flowbite-svelte-icons';
-    let popupModal = false;
+	/** @type {import('./$types').PageData} */
+	import { supabaseClient } from '$lib/supabase';
+	import { onMount } from 'svelte';
+	import { AccordionItem, Accordion } from 'flowbite-svelte';
+	let popupModal = false;
 
-    let userData;
-    /**
+	let userData;
+	/**
 	 * @type {any}
 	 */
-    let customerData;
-    let user_id;
+	let customerData;
+	/**
+	 * @type {string | undefined}
+	 */
+	let user_id; //Customer ID
+	/**
+	 * @type {any}
+	 */
+	let orders = [];
+	/**
+	 * @type {any}
+	 */
+	let orderitems = [];
 
-    onMount(async () => {
-        console.log('im here start');
-        customerData = await fetchCustomerData();
-        console.log(customerData);
-    });
+	onMount(async () => {
+		console.log('im here start');
+		const userLog = await supabaseClient.auth.getUser();
+		user_id = userLog.data.user?.id;
+		customerData = await fetchCustomerData();
+		orders = await fetchOrders();
+		for (let order of orders) {
+            console.log(order);
+			order.orderitems = await fetchOrderItems(order.orderid);
+		}
+		console.log(customerData);
+		console.log(orders);
+	});
 
-    async function fetchCustomerData(){
-        const userLog = await supabaseClient.auth.getUser();
-        user_id = userLog.data.user?.id;
-        console.log('im here');
+	async function fetchCustomerData() {
+		const userLog = await supabaseClient.auth.getUser();
+		user_id = userLog.data.user?.id;
+		console.log('im here');
 
-        const {data, error} = await supabaseClient.from('Customer')
-        .select('*')
-        .eq('user_id', user_id);
+		const { data, error } = await supabaseClient
+			.from('customer')
+			.select('*')
+			.eq('user_id', user_id);
 
-        if (error){
-            console.error('Error fetching customer name: ', error);
-        } else if (data && data.length > 0){
-            customerData = data[0];
-            console.log('enter');
-        }
-        return customerData;
-    }
+		if (error) {
+			console.error('Error fetching customer name: ', error);
+		} else if (data && data.length > 0) {
+			customerData = data[0];
+			console.log('enter');
+		}
+		return customerData;
+	}
 
-    async function deleteOrderItem(){
-        alert('Delete successful')
-    }
+	async function fetchOrders() {
+		console.log('fetching orders');
+		const { data: order, error } = await supabaseClient
+			.from('cusorder')
+			.select('*')
+			.eq('customerid', customerData.customerid)
+			.eq('cartstatus', 'completed');
 
-    //export let data;
+		if (error) {
+			console.error('Error fetching orders: ', error);
+		} else if (order && order.length > 0) {
+			orders = order;
+		}
+		console.log('done fetching');
+		return order;
+	}
 
+	/**
+	 * @param {any} orderid
+	 */
+	async function fetchOrderItems(orderid) {
+		console.log('fetching order items');
+		console.log(orderid);
+		const { data: orderitem, error } = await supabaseClient
+			.from('orderitem')
+			.select('*')
+			.eq('orderid', orderid);
+
+		if (error) {
+			console.error('Error fetching order items: ', error);
+		} else if (orderitem && orderitem.length > 0) {
+            console.log('something',orderitems);
+			orderitems = orderitem;
+		}
+		console.log('done fetching');
+        console.log(orderitems.orderitemid);
+		return orderitem || [];
+	}
+
+	//export let data;
 </script>
-<div class="topSpace"></div>
-<div class="titleBar">
-        <h1>Hello World</h1>
-</div>
 
-<div>
-    <TrashBin deleteOrderItem={deleteOrderItem} />
-</div>
+<div class="page-container min-h-[100vh] overflow-x-hidden">
+	<div class="flex flex-col items-center h-16 bg-gray-900">
+		<div class="font-bold text-2xl text-white w-full h-9 flex items-center justify-center">
+			<h1>Hi, Shopper.</h1>
+		</div>
 
-<style>
-    .titleBar {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 30px;
-        background-color: orange;
-    }
-    .topSpace{
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 30px;
-    }
-</style>
+		<div class="text-base text-white w-full h-8 flex items-center justify-center">
+			<h1>What will your next order be?</h1>
+		</div>
+	</div>
+	<div>
+		{#each orders as order (order.orderid)}
+			<Accordion>
+				<AccordionItem>
+					<span slot="header">{order.orderid}</span>
+					<p class="mb-2 text-gray-500 dark:text-gray-400">
+						
+					</p>
+					<p class="text-gray-500 dark:text-gray-400">
+						Check out this guide to learn how to <a
+							href="/"
+							target="_blank"
+							rel="noreferrer"
+							class="text-blue-600 dark:text-blue-500 hover:underline"
+						>
+							get started
+						</a>
+						and start developing websites even faster with components on top of Tailwind CSS.
+					</p>
+				</AccordionItem>
+			</Accordion>
+		{/each}
+	</div>
+</div>
