b4a6bd867023cd0b504057b9fd071ad4e71f169a
SuzySi <1221302230@student.mmu.edu.my>
2024-02-03 00:16:58 +0800

    sys manager dashboard

--- src/routes/client/dashboard/manager/+page.svelte
+++ src/routes/client/dashboard/manager/+page.svelte
@@ -1,23 +1,49 @@
-<div class="dashboard">
-    <h1>Welcome back, System Manager name</h1>
+
+  <div class="page-container min-h-[100vh] overflow-x-hidden bg-white dark:bg-stone-500">
+    <div class="header h-20 bg-gray-900 px-6 flex flex-row justify-between">
+  
+  </div>
+
+  <div class="dashboard">
+    <h1>Welcome back, System Manager </h1> <!--add system manager name-->
+    <h1>What would you like to do today?</h1>
+    
+  </div>
+
+  <div class= "buttons">
     <button>View Vendors Sales Report</button>
     <button>View Delivery Report</button>
     <button>Refund Undelivered/Rejected Orders</button>
     <button>View Customer Order History</button>
   </div>
+  </div>
 
+      
   <style>
     .dashboard {
+      font-size: 40px;
+      font-family: 'Molengo', sans-serif;
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: center;
-      gap: 1rem;
+      gap: 3rem;
+    
+    }
+    .buttons {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 3rem;
     }
     
     button {
-      padding: 0.5rem 1rem;
-      background-color: orange;
+      padding: 30px 50px;
+      font-size: 20px;
+      font-family: 'Inria Sans', sans-serif;
+      background-color: #EF562F;
+      border-radius: 10px; 
       border: none;
       color: white;
       cursor: pointer;
