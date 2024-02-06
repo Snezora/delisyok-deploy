f4d0f80754e378bef26474518f4d43ea87975cf6
Ren <125994044+R3nT@users.noreply.github.com>
2024-02-07 00:23:32 +0800

    test - redirect issue on vercel

--- src/routes/client/dashboard/customer/[vendorMenu]/cart/checkout/+page.svelte
+++ src/routes/client/dashboard/customer/[vendorMenu]/cart/checkout/+page.svelte
@@ -309,8 +309,8 @@
 			if (error2) {
 				console.error('Error updating cart status: ', error2);
 			} else {
-				alert('Your Order has been Submitted. Please check order history for updates!');
 				setTimeout(() => {
+					alert('Your Order has been Submitted. Please check order history for updates!');
 					window.location.href = '/client/dashboard/customer';
 				}, 2000); // Delay the redirection for 2000 milliseconds (2 seconds)
 			}
